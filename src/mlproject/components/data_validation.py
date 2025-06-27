855779c6d03044fdab3f2b3b73fb0edaf9001a2c
Vegadhardik7 <vegadhardik7@gmail.com>
2025-06-27 15:07:53 +0530

    fix: data transformation added | CC-007

--- src/mlproject/components/data_validation.py
+++ src/mlproject/components/data_validation.py
@@ -8,29 +8,28 @@ class DataValidation:
         self.config = config
 
 
-    def validate_all_columns(self)-> bool:
-            try:
+    def validate_all_columns(self) -> bool:
+        try:
+            validation_status = None
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+            all_schema = self.config.all_schema.keys()
+
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    break
+
+            if validation_status is None:
                 validation_status = True
 
-                data = pd.read_csv(self.config.unzip_data_dir)
-                all_cols = list(data.columns)
+            with open(self.config.STATUS_FILE, 'w') as f:
+                f.write(f"Validation status: {validation_status}")
 
-                all_schema = self.config.all_schema.keys()
+            return validation_status
 
-                
-                for col in all_cols:
-                    if col not in all_schema:
-                        validation_status = False
-                        with open(self.config.STATUS_FILE, 'w') as f:
-                            f.write(f"Validation status: {validation_status}")
-                    else:
-                        validation_status = True
-                        with open(self.config.STATUS_FILE, 'w') as f:
-                            f.write(f"Validation status: {validation_status}")
-
-                return validation_status
-            
-            except Exception as e:
-                raise e
+        except Exception as e:
+            raise e
 
 
